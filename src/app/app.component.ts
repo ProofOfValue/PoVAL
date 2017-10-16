72b158c40246b58d5dd7aeb5d8588e3dc4037a2f
Brandon McFarland <branmcf@gmail.com>
2017-10-15 20:42:47 -0500

    Added api response to app.component.html. Added code to connect to api in app.component.ts and app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PoVAL';
+  private apiUrl = 'https://poval-api.herokuapp.com/api';
+  data: any = {};
+
+  constructor(private http: Http) {
+    console.log('Hello POVAL user!');
+    this.hitAPI();
+    this.hitRoot();
+  }
+
+  hitAPI() {
+    return this.http.get(this.apiUrl)
+      .map((res: Response) => res.json())
+  }
+
+  hitRoot() {
+    this.hitAPI().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    })
+  }
+
 }
